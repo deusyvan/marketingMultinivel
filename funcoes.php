517968a09408d5a207e5ef04350165245cfc9acb
deusyvan <deusyvan@gmail.com>
2018-06-02 09:45:55 -0300

    Criando nossa classe funções

---
+++ funcoes.php
@@ -0,0 +1,14 @@
+<?php
+function  listar($id){
+    $lista = array();
+    global $pdo;
+    $sql = $pdo->prepare("SELECT nome FROM usuarios WHERE id_pai = :id");
+    $sql->bindValue(":id", $id);
+    $sql->execute();
+    
+    if ($sql->rowCount() > 0) {
+        $lista = $sql->fetchAll();
+    } 
+    
+    return $lista;
+}
