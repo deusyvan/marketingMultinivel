0b1f6c930b32fd41b8a54523c2a0e56cb7a783ee
deusyvan <deusyvan@gmail.com>
2018-06-02 02:05:48 -0300

    Insere o usuário se email não existir

--- cadastro.php
+++ cadastro.php
@@ -13,7 +13,17 @@
        $sql->bindValue(":email", $email);
        $sql->execute();
        
-       
+       if ($sql->rowCount() == 0) {
+           $sql = $pdo->prepare("INSERT INTO usuarios (id_pai, nome. email, senha) 
+                                VALUES(:id_pai, :nome, :email, :senha)");
+           $sql->bindValue(":id_pai", $id_pai);
+           $sql->bindValue(":nome", $nome);
+           $sql->bindValue(":email", $email);
+           $sql->bindValue(":senha", $senha);
+           $sql->execute();
+           
+           
+       }
        
         
     }
