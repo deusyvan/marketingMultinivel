8375f378d354e95d4d748fb4ed8321eca813f5ab
deusyvan <deusyvan@gmail.com>
2018-06-02 14:16:05 -0300

    Atualizador das patentes dos usuários

--- atualizar.php
+++ atualizar.php
@@ -13,5 +13,24 @@ if($sql->rowCount() > 0){
     }
 }
 
-echo '<pre>';
-print_r($usuarios);
+$sql = "SELECT * FROM patentes ORDER BY minimo DESC";
+$sql = $pdo->query($sql);
+$patentes = array();
+
+if ($sql->rowCount() > 0) {
+    $patentes = $sql->fetchAll();
+}
+
+foreach ($usuarios as $usuario) {
+    foreach ($patentes as $patente) {
+        if(intval($usuario['filhos']) >= intval($patente['minimo'])) {
+            $sql = "UPDATE usuarios SET patente = :patente WHERE id = :id";
+            $sql = $pdo->prepare($sql);
+            $sql->bindValue(":patente", $patente['id']);
+            $sql->bindValue(":id", $usuario['id']);
+            $sql->execute();
+            
+            break;
+        }
+    }
+}
