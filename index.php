8ac5c65e9d7eb8bf4591a675c55b7fd39c90ae02
deusyvan <deusyvan@gmail.com>
2018-06-01 23:41:18 -0300

    Criamos o formulario de login buscando os campos de e-mail e senha

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php 
     session_start();
     require 'config.php';
-
-?>
+    
+    //verificando se existe um usuario logado
+    if (empty($_SESSION['mmnlogin'])) {
+        header("Location: login.php");
+        exit;
+    }
