5f35e775901db9f4488fc0e52ec066bbe5c64217
deusyvan <deusyvan@gmail.com>
2018-06-02 01:39:19 -0300

    Mostra o usuário na tela após logado

--- index.php
+++ index.php
@@ -7,3 +7,25 @@
         header("Location: login.php");
         exit;
     }
+    
+    //Recuperando o id
+    $id = $_SESSION['mmnlogin'];
+    
+    //buscando o nome pelo id
+    $sql = $pdo->prepare("SELECT nome FROM usuarios WHERE id = :id");
+    $sql->bindValue(":id", $id);
+    $sql->execute();
+    
+    //verificando se o id existe
+    if ($sql->rowCount() > 0) {
+        //traz o nome
+        $sql = $sql->fetch();
+        $nome = $sql['nome'];
+    } else {
+        header("Location: login.php");
+        exit;
+    }
+    
+?>
+<h1>Sistema de Marketing Multinível</h1>
+<h2>Usuário logado: <?php echo $nome ?></h2>
